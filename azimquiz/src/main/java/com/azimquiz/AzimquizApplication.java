575729861f14f74d510dcbbbbe2695cd8a870ac5
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-05-17 05:07:59 +0300

    Initial commit
    I initialize the spring boot project using spring initializer then I connected the database in application properties. Also, I made frontend project with Angular and installed the required packages into the project.

---
+++ azimquiz/src/main/java/com/azimquiz/AzimquizApplication.java
@@ -0,0 +1,13 @@
+package com.azimquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzimquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzimquizApplication.class, args);
+	}
+
+}
---
+++ azimquiz/src/test/java/com/azimquiz/AzimquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azimquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzimquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
