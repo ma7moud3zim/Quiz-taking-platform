d521ec824dbbfb82b0ef2e5b968785ae071f1515
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-05-18 07:34:34 +0300

    Made the sign up form

--- azimquizfe/src/app/modules/auth/signup/signup.ts
+++ azimquizfe/src/app/modules/auth/signup/signup.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared-module';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
 })
-export class Signup {}
+export class Signup {
+  constructor(private fb: FormBuilder,
+    private message: NzMessageService,
+    private router: Router
+  ) {
+
+
+  }
+
+    validateForm!: FormGroup;
+
+    ngOnInit(){
+      this.validateForm = this.fb.group({
+        email: [null, [Validators.required, Validators.email]],
+        password: [null, [Validators.required]],
+        name: [null, [Validators.required]],
+      });
+    }
+
+    submitForm(){  }
+}
